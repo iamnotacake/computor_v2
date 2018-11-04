c2f320acfa960cc0de9634de92fa1c9066b70afc
Anatoliy Lischynsky <iamnotacake@protonmail.com>
2018-11-04 17:56:13 +0200

    Add Context as hashmap of String:Value

---
+++ computor_v2/src/lib.rs
@@ -0,0 +1,5 @@
+use std::collections::HashMap;
+
+pub enum Value {}
+
+pub type Context = HashMap<String, Value>;
--- computor_v2/src/main.rs
+++ computor_v2/src/main.rs
@@ -1,18 +1,20 @@
 extern crate computor_v2;
 extern crate rustyline;
 
+use computor_v2::Context;
 use rustyline::error::ReadlineError;
 
-fn process_line(line: &str) {
+fn process_line(line: &str, context: &mut Context) {
     // TODO
 }
 
 fn main() {
     let mut rl = rustyline::Editor::<()>::new();
+    let mut context = Context::new();
 
     loop {
         match rl.readline("> ") {
-            Ok(line) => process_line(&line),
+            Ok(line) => process_line(&line, &mut context),
             Err(ReadlineError::Interrupted) => {}
             Err(ReadlineError::Eof) => break,
             Err(err) => panic!("{}", err),
