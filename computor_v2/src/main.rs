525e15d910f33c1c8dbd09de31c38d04b038364e
Anatoliy Lischynsky <iamnotacake@protonmail.com>
2018-11-04 17:11:28 +0200

    Add readline, almost empty grammar

--- computor_v2/src/main.rs
+++ computor_v2/src/main.rs
@@ -1,3 +1,21 @@
+extern crate computor_v2;
+extern crate rustyline;
+
+use rustyline::error::ReadlineError;
+
+fn process_line(line: &str) {
+    // TODO
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut rl = rustyline::Editor::<()>::new();
+
+    loop {
+        match rl.readline("> ") {
+            Ok(line) => process_line(&line),
+            Err(ReadlineError::Interrupted) => {}
+            Err(ReadlineError::Eof) => break,
+            Err(err) => panic!("{}", err),
+        }
+    }
 }
